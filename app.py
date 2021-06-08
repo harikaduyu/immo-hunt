f818e1a53e69536a067112f8cc669e56db720845
Harika <h.duyu@rebuy.com>
2021-06-08 20:29:40 +0200

    remove spaces

--- app.py
+++ app.py
@@ -9,22 +9,16 @@ def build_immo_url_from_config(cfg):
   """Builds immobilienscout url using config values to set request parameters"""
 
   mit_balkon = cfg.mit_balkon if hasattr(cfg, 'mit_balkon') else ""
-
   url = f"{cfg.url}{cfg.city}/{cfg.city}/wohnung{mit_balkon}-mieten?"
 
   #Build parameters
   url += f"haspromotion={cfg.wbs}&"  if hasattr(cfg, 'wbs') and cfg.wbs is not None else ""
-
   url += f"numberofrooms={cfg.num_of_rooms}&" if hasattr(cfg, 'wbs') and cfg.num_of_rooms is not None else ""
-
   price_from =  cfg.price_from if (hasattr(cfg, 'price_from') and cfg.price_from is not None) else ""
   price_up_to = cfg.price_up_to if (hasattr(cfg, 'price_up_to') and cfg.price_up_to is not None) else ""
   url += f"price={price_from}-{price_up_to}&"  
-
   url += f"livingspace={cfg.livingspace}&" if  hasattr(cfg, 'livingspace') and cfg.livingspace  else ""
-
   url += f"price_type={cfg.price_type}&" if hasattr(cfg, 'price_type') and cfg.price_type  else ""
-
   url += f"floor={cfg.floor_from}" if hasattr(cfg, 'floor_from') and cfg.floor_from  else ""
 
   return url
@@ -45,7 +39,7 @@ def main():
 
   url = build_immo_url_from_config(cfg.immoscout)
   result_list = get_result_list(url)
-  
+
 
 
 if __name__ == '__main__':
