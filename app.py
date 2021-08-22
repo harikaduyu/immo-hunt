3ea366163ade8169b66cc01723cab1d172d95fe9
Harika <h.duyu@rebuy.com>
2021-08-22 09:19:15 +0200

    creates a seperate immoscout agent

--- app.py
+++ app.py
@@ -1,95 +1,27 @@
 import yaml
 from box import Box
-import requests
-import json
 import time
 import telegram
 
-def build_immo_url_from_config(cfg):
-  """Builds immobilienscout url using config values to set request parameters"""
+from immoscout_agent import ImmoScout
 
-  with_balcony =  "-mit-balkon" if hasattr(cfg, 'with_balcony') and cfg.with_balcony else ""
-  url = f"{cfg.url}{cfg.city}/{cfg.city}/wohnung{with_balcony}-mieten?"
-
-  #Build parameters
-  url += f"haspromotion={cfg.wbs}&"  if hasattr(cfg, 'wbs') and cfg.wbs is not None else ""
-  url += f"shape={cfg.shape}&"  if hasattr(cfg, 'shape') and cfg.shape is not None else ""
-  url += f"numberofrooms={cfg.num_of_rooms}&" if hasattr(cfg, 'wbs') and cfg.num_of_rooms is not None else ""
-  price_from =  cfg.price_from if (hasattr(cfg, 'price_from') and cfg.price_from is not None) else ""
-  price_up_to = cfg.price_up_to if (hasattr(cfg, 'price_up_to') and cfg.price_up_to is not None) else ""
-  url += f"price={price_from}-{price_up_to}&"  
-  url += f"livingspace={cfg.livingspace}&" if  hasattr(cfg, 'livingspace') and cfg.livingspace  else ""
-  url += f"price_type={cfg.price_type}&" if hasattr(cfg, 'price_type') and cfg.price_type  else ""
-  url += f"floor={cfg.floor_from}" if hasattr(cfg, 'floor_from') and cfg.floor_from  else ""
-  url += "&sorting=2"
-  return url
-
-def get_result_list(url):
-  """Returns the available listings as an array of raw results"""
-  r = requests.post(url)
-  content = json.loads(r.content)
-  result_list = content["searchResponseModel"]["resultlist.resultlist"]["resultlistEntries"][0]["resultlistEntry"]
-  return result_list
-
-def parse_result(result):
-  """Parses the relevant info from the result json to a string for Telegram message"""
-  res = result['resultlist.realEstate']['address']['quarter'] +'\n'
-  res += "Total Rent: " + str(result['resultlist.realEstate']["calculatedTotalRent"]["totalRent"]["value"]) +'Eur\n'
-  attributes = result['attributes'][0]['attribute']
-  for a in attributes:
-    res += str(a['label']) + ": " + str(a['value']) +'\n'
-  res += "https://www.immobilienscout24.de/expose/" + result['@id']
-  return res
-
-
-def get_message_suggestion(result,cfg):
-  """Prepares a message suggestions to send"""
-  contact = result['resultlist.realEstate']['contactDetails']
-  salutation = "Sehr geehrte"
-  if 'lastname' in contact:
-    salutation +=  " Frau " if contact['salutation'] == "FEMALE" else "r Herr "
-    salutation += contact['lastname']
-  else:
-    salutation += " Damen und Herren"
-  address = result['resultlist.realEstate']['address']['description']['text']
-  text = f"{salutation},\n{cfg.text_before_address} {address} {cfg.text_after_address}" 
-  print(text)
-  return text
-
-def log_result(result):
-  """Logs the relevant info from the result"""
-  print("id: "+result['@id'])
-  print("title")
-  print(result['resultlist.realEstate']['title'])
-  print("address")
-  print(result['resultlist.realEstate']['address'])
-  print("contact")
-  print(result['resultlist.realEstate']['contactDetails'])
-
-  # print(json.dumps(result, sort_keys=False, indent=4))
 
 def main():
   with open("config.yaml", "r",  encoding='utf-8') as ymlfile:
-    cfg = Box(yaml.safe_load(ymlfile))
+    config = Box(yaml.safe_load(ymlfile))
 
   # Don't get notified for the existing offers
-  first_run = True
-  m = set()
-  url = build_immo_url_from_config(cfg.immoscout)
-  bot = telegram.Bot(token=cfg.telegram.token)
-  bot.sendMessage(chat_id = cfg.telegram.chat_id, text = "Start")
+  immoscout_agent = ImmoScout(config.immoscout)
+  bot = telegram.Bot(token=config.telegram.token)
+  bot.sendMessage(chat_id = config.telegram.chat_id, text = "Start")
   while True:
-    result_list = get_result_list(url)
-    for result in result_list:
-      if result['@id'] not in m:
-        if not first_run:
-          res = parse_result(result) 
-          bot.sendMessage(chat_id = cfg.telegram.chat_id, text = res )        
-          log_result(result)
-          message_suggestion = get_message_suggestion(result,cfg.message)
-          bot.sendMessage(chat_id = cfg.telegram.chat_id, text = message_suggestion )  
-        m.add(result['@id'])
-    first_run = False
+    for immo_result in immoscout_agent.get_new_ads_results():
+      parsed_result = immoscout_agent.parse_result(immo_result) 
+      bot.sendMessage(chat_id = config.telegram.chat_id, text = parsed_result )        
+      immoscout_agent.log_result(immo_result)
+      message_text = immoscout_agent.get_message_suggestion(immo_result)
+      bot.sendMessage(chat_id = config.telegram.chat_id, text = message_text )  
+
     time.sleep(60)
 
 if __name__ == '__main__':
---
+++ immoscout_agent.py
@@ -0,0 +1,88 @@
+import requests
+import json
+
+
+class ImmoScout:
+  def __init__(self, config):
+    self.config = config
+    self.url = self._build_immo_url_from_config()
+    self.map = set()
+    self.first_run = True
+
+  def _build_immo_url_from_config(self):
+    """Builds immobilienscout url using config values to set request parameters"""
+    config = self.config
+    if hasattr(config, 'whole_url') and config.whole_url:
+      return config.whole_url
+    with_balcony =  "-mit-balkon" if hasattr(config, 'with_balcony') and config.with_balcony else ""
+    url = f"{config.url}{config.city}/{config.city}/wohnung{with_balcony}-mieten?"
+
+    #Build parameters
+    url += f"haspromotion={config.wbs}&"  if hasattr(config, 'wbs') and config.wbs is not None else ""
+    url += f"shape={config.shape}&"  if hasattr(config, 'shape') and config.shape is not None else ""
+    url += f"numberofrooms={config.num_of_rooms}&" if hasattr(config, 'wbs') and config.num_of_rooms is not None else ""
+    price_from =  config.price_from if (hasattr(config, 'price_from') and config.price_from is not None) else ""
+    price_up_to = config.price_up_to if (hasattr(config, 'price_up_to') and config.price_up_to is not None) else ""
+    url += f"price={price_from}-{price_up_to}&"  
+    url += f"livingspace={config.livingspace}&" if  hasattr(config, 'livingspace') and config.livingspace  else ""
+    url += f"price_type={config.price_type}&" if hasattr(config, 'price_type') and config.price_type  else ""
+    url += f"floor={config.floor_from}" if hasattr(config, 'floor_from') and config.floor_from  else ""
+    url += "&sorting=2"
+    return url
+
+  def _get_raw_result_list(self):
+    """Returns the available listings as an array of raw results"""
+    response = requests.post(self.url)
+    immoscout_ads_content = json.loads(response.content)
+    raw_result_list = immoscout_ads_content["searchResponseModel"]["resultlist.resultlist"]["resultlistEntries"][0]["resultlistEntry"]
+    return raw_result_list
+
+  def parse_result(result):
+    """Parses the relevant info from the result json to a string for Telegram message"""
+    parsed_result = result['resultlist.realEstate']['address']['quarter'] +'\n'
+    parsed_result += "Total Rent: " + str(result['resultlist.realEstate']["calculatedTotalRent"]["totalRent"]["value"]) +'Eur\n'
+    attributes = result['attributes'][0]['attribute']
+    for attribute in attributes:
+      parsed_result += str(attribute['label']) + ": " + str(attribute['value']) +'\n'
+    parsed_result += "https://www.immobilienscout24.de/expose/" + result['@id']
+    return parsed_result
+
+
+  def get_message_suggestion(self,result):
+    """Prepares a message suggestions to send"""
+    message_config =self.config.message
+    contact = result['resultlist.realEstate']['contactDetails']
+    salutation = "Sehr geehrte"
+    if 'lastname' in contact:
+      salutation +=  " Frau " if contact['salutation'] == "FEMALE" else "r Herr "
+      salutation += contact['lastname']
+    else:
+      salutation += " Damen und Herren"
+    address = result['resultlist.realEstate']['address']['description']['text']
+    message_text = f"{salutation},\n{message_config.text_before_address} {address} {message_config.text_after_address}" 
+    print(message_text)
+    return message_text
+
+  def log_result(result):
+    """Logs the relevant info from the result"""
+    print("id: "+result['@id'])
+    print("title")
+    print(result['resultlist.realEstate']['title'])
+    print("address")
+    print(result['resultlist.realEstate']['address'])
+    print("contact")
+    print(result['resultlist.realEstate']['contactDetails'])
+
+
+  def get_new_ads_results(self):
+    """Gets new ads starting from the second run, ignores the already existing ads """
+    results = []
+    raw_result_list = self._get_raw_result_list()
+    for result in raw_result_list:
+      if result['@id'] not in self.map:
+        if not self.first_run:
+          results.append(result)
+        self.map.add(result['@id'])
+    self.first_run = False
+    return results
+
