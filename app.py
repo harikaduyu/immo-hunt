24bbb58d81c6c9f576a06cef73b15e847c15987a
Harika <h.duyu@rebuy.com>
2021-08-22 17:09:50 +0200

    remove comment

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ def main():
   with open("config.yaml", "r",  encoding='utf-8') as ymlfile:
     config = Box(yaml.safe_load(ymlfile))
 
-  # Don't get notified for the existing offers
   immoscout_agent = ImmoScout(config.immoscout)
   bot = telegram.Bot(token=config.telegram.token)
   bot.sendMessage(chat_id = config.telegram.chat_id, text = "Start")
@@ -21,7 +20,6 @@ def main():
       immoscout_agent.log_result(immo_result)
       message_text = immoscout_agent.get_message_suggestion(immo_result)
       bot.sendMessage(chat_id = config.telegram.chat_id, text = message_text )  
-
     time.sleep(60)
 
 if __name__ == '__main__':
