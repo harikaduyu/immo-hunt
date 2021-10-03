210bbb7a7467c0a24ac1513fe0369e2986bc1b21
Harika <harika.duyu@gmail.com>
2021-10-03 10:13:35 +0200

    log url

--- app.py
+++ app.py
@@ -13,13 +13,15 @@ def main():
   immoscout_agent = ImmoScout(config.immoscout)
   bot = telegram.Bot(token=config.telegram.token)
   bot.sendMessage(chat_id = config.telegram.chat_id, text = "Start")
+  print("Running using url : " + immoscout_agent.url)
   while True:
     for immo_result in immoscout_agent.get_new_ads_results():
+      print(immo_result)
       parsed_result = immoscout_agent.parse_result(immo_result) 
       bot.sendMessage(chat_id = config.telegram.chat_id, text = parsed_result )        
       immoscout_agent.log_result(immo_result)
       message_text = immoscout_agent.get_message_suggestion(config.message,immo_result)
-      bot.sendMessage(chat_id = config.telegram.chat_id, text = message_text )  
+      bot.sendMessage(chat_id = config.telegram.chat_id, text = message_text)  
     time.sleep(60)
 
 if __name__ == '__main__':
